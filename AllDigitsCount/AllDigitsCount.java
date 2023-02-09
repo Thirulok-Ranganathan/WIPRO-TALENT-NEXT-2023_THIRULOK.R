12b1645ee537ceb6826a64eb4182eaec3c0f68b7
THIRULOK <73477145+Thirulok-Ranganathan@users.noreply.github.com>
2023-02-09 09:41:04 +0530

    Add files via upload

---
+++ AllDigitsCount/AllDigitsCount.java
@@ -0,0 +1,10 @@
+package AllDigitsCount;
+
+public class AllDigitsCount {
+    public static void main(String[] args) {
+        int input1=234;
+        String num=Integer.toString(input1);
+        int len=num.length();
+        System.out.println(len);
+    }
+}
